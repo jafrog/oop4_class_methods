3b03a19273d6ea56cf0c77c979c5f17435d48990
jafrog <howeveririna@gmail.com>
2016-07-28 12:00:23 +0100

    Adds ::name and ::build method to the Team class

--- lib/team.rb
+++ lib/team.rb
@@ -1,6 +1,27 @@
 require_relative "player"
 
 class Team
+  def self.name=(name)
+    @name = name
+  end
+
+  def self.name
+    @name
+  end
+
+  def self.build(players)
+    jammer = players.find(&:jammer?)
+    return unless jammer
+
+    pivot = players.find(&:pivot?)
+    return unless pivot
+
+    blockers = players.reject { |player| player.jammer? || player.pivot? }
+    return unless blockers.count == 3
+
+    new(name, jammer, pivot, blockers)
+  end
+
   def self.build_default(player_class = Player)
     jammer   = player_class.new("Bambi Brainbox", jammer: true)
     pivot    = player_class.new("The Rib Jammer", pivot: true)
@@ -8,7 +29,7 @@ class Team
     blocker2 = player_class.new("Star Tsunami")
     blocker3 = player_class.new("Anastasia Dragonfly")
 
-    new("Gotham bombs", jammer, pivot, [blocker1, blocker2, blocker3])
+    new(name, jammer, pivot, [blocker1, blocker2, blocker3])
   end
 
   attr_reader :jammer, :pivot, :blockers
--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -8,5 +8,17 @@ describe Team do
     expect(team.players.count).to eq 5
   end
 
-  xit "builds a team from a list of players"
+  it "builds a team from a list of players" do
+    jammer = double("Player", jammer?: true, pivot?: false)
+    pivot = double("Player", jammer?: false, pivot?: true)
+    blockers = Array.new(3) { double("Player", jammer?: false, pivot?: false) }
+    players = [jammer, pivot, *blockers]
+
+    team = described_class.build(players)
+    expect(team.players.count).to eq 5
+    expect(team.players.find(&:jammer?)).to eq jammer
+
+    expect(described_class.build([])).to be_nil
+    expect(described_class.build(blockers)).to be_nil
+  end
 end
